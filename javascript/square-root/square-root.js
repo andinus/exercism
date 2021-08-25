00ca21200f8ab83e2614c7607e8617966f36ea50
Andinus <andinus@nand.sh>
2021-08-25 20:12:07 +0530

    JS: Remove Solution for square-root
    Cannot use built-in function.

--- javascript/square-root/square-root.js
+++ javascript/square-root/square-root.js
@@ -1,3 +1,6 @@
 export const squareRoot = (num) => {
-    return Math.sqrt(num);
+    // The idea is not to use the built-in javascript functions such
+    // as Math.sqrt(x), x ** 0.5 or x ** (1/2), it's to build your
+    // own.
+    return num;
 };
