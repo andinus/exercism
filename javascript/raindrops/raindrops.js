46d81156b7bad4bef2f4443df6e9df4f03303c56
Andinus <andinus@nand.sh>
2021-09-05 16:38:00 +0530

    JS: Raindrops: Move rules to a const

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -1,17 +1,19 @@
 'use strict';
 
 export const convert = (number) => {
-    let drops = "";
-
-    [
+    const rules = [
         {factor: 3, result: 'Pling'},
         {factor: 5, result: 'Plang'},
         {factor: 7, result: 'Plong'}
-    ].forEach(drop => {
-         if (number % drop.factor === 0)
-             drops += drop.result;
-     });
+    ];
 
-    if (drops === "") drops += number;
+    let drops = "";
+    rules.forEach(drop => {
+        if (number % drop.factor === 0)
+            drops += drop.result;
+    });
+
+    if (drops === "")
+        drops += number;
     return drops;
 };
