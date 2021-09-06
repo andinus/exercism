2c3463a677d01dfc027bc518de91eeeaebc14a5b
Andinus <andinus@nand.sh>
2021-09-06 18:33:47 +0530

    Clojure: Raindrops: Improve solution
    Avoids logic repetition.

--- clojure/raindrops/src/raindrops.clj
+++ clojure/raindrops/src/raindrops.clj
@@ -1,15 +1,11 @@
 (ns raindrops)
 
 (defn convert [number]
-  (def drops "")
-
-  (if (== (mod number 3) 0)
-    (def drops (str drops "Pling")))
-  (if (== (mod number 5) 0)
-    (def drops (str drops "Plang")))
-  (if (== (mod number 7) 0)
-    (def drops (str drops "Plong")))
-
-  (if (= drops "")
-    (def drops (str number)))
-  drops)
+  (let [drops (apply str
+                     (map (fn [factor string]
+                            (if (== (mod number factor) 0)
+                              string nil))
+                          [3 5 7]
+                          ["Pling" "Plang" "Plong"]))]
+    (if (= drops "")
+      (str number) drops)))
