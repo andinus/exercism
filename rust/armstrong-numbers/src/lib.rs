09e7fe7c4bae239bda9290d3dca0fe51bee9211e
Andinus <andinus@nand.sh>
2021-09-08 23:02:26 +0530

    Rust: Armstrong Numbers: Add solution

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    if num == 0 {
+        return true;
+    }
+
+    let len = (num as f32).log10() as u32 + 1;
+
+    let mut arms = 0;
+    let mut tmp = num;
+    while tmp != 0 {
+        let rem = tmp % 10;
+        arms += rem.pow(len);
+        tmp /= 10
+    }
+
+    return arms == num;
 }
