62fd5eceb73c4a5baf484738b8432e08537c5e17
Andinus <andinus@nand.sh>
2023-07-16 20:44:50 +0530

    Rust: Anagram: Improve performance
    word should be lower-cased, sorted only once.

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,23 +1,32 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let word_lower = word.to_lowercase();
+    let word_lexical = lexical_order(&word_lower);
+
     possible_anagrams
         .iter()
         .filter(|w| {
-            w.len() == word.len()
-                // a word cannot be an anagram of itself.
-                && w.to_lowercase() != word.to_lowercase()
-                // lexical orders of w should match if it's an anagram of word.
-                && lexical_order(w) == lexical_order(word)
+            // w's length should be same as word's for it to be a possible
+            // anagram.
+            if w.len() != word.len() {
+                return false;
+            }
+
+            let w_lower = w.to_lowercase();
+            // a word cannot be an anagram of itself.
+            w_lower != word_lower
+            // lexical orders of w should match if it's an anagram of word.
+                && word_lexical == lexical_order(&w_lower)
         })
         .map(|anagram| *anagram)
         .collect()
 }
 
-// lexical_order takes a string and returns Vec<char> where the chars are sorted
-// in lexical ordering. Refer sort_unstable for details.
+// lexical_order takes a string (must be lowercased) and returns Vec<char> where
+// the chars are sorted in lexical ordering. Refer sort_unstable for details.
 fn lexical_order(word: &str) -> Vec<char> {
-    let mut result: Vec<char> = word.to_lowercase().chars().collect();
+    let mut result: Vec<char> = word.chars().collect();
     result.sort_unstable();
     result
 }
