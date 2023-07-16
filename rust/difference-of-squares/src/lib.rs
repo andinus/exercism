191040ebf4aa159e947bd213e7a7e1f13e8b4a16
Andinus <andinus@nand.sh>
2023-07-16 20:44:13 +0530

    Rust: Difference of Squares: Optimise calculations

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    (1..=n).fold(0, |acc, x| acc + x).pow(2)
+    (n * (n + 1) / 2).pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..=n).map(|x| x.pow(2)).fold(0, |acc, x| acc + x)
+    n * (n + 1) * ((n * 2) + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
